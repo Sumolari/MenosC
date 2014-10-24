28451ea94b8a78910e02f7dcf76e5b8408678ead
Lluís Ulzurrun de Asanza i Sàez <me@llu.is>
2014-10-24 20:59:50 +0200

    Added tests for syntactic analyzer.

--- LAB/tests.6.c
+++ LAB/tests.6.c
@@ -1,3 +1,34 @@
-int numero 7;
-numero = 20;
-if(numero>=4)numero=3;else numero=5+5;
+{
+	int numero[7 ];
+	numero = 20;
+
+	bool boleano;
+	boleano = false;
+	bool boleano[57];
+
+	if ( 5 ) {} else {}
+
+	if ( 5 ) numero = 3; else numero = 5;
+
+	if ( numero >= 4 )
+		numero = 3;
+	else
+		numero = 5 + 5;
+
+	numero = 30;
+
+	if ( numero >= 4 )
+		numero = 3;
+	else
+		numero = 5 + 5;
+
+	if (numero>=4)
+		numero = 3;
+	else
+		numero = 5+5;
+
+	if(numero>=4)numero=3;
+	else numero=5+5;
+
+	if(numero>=4)numero=3;else numero=5+5;
+}
