215da54d8b2ca87785af5f652d7e28d46fdec91d
Lluís Ulzurrun de Asanza i Sàez <me@llu.is>
2014-12-03 14:35:53 +0100

    Code will be written to a file.

--- LAB/src/principal.c
+++ LAB/src/principal.c
@@ -5,6 +5,7 @@
 #include <string.h>
 #include "../include/header.h"
 #include "../include/libtds.h"
+#include "../include/libgci.h"
 
 int verbosidad = FALSE;             /* Flag para saber si se desea una traza */
 int numErrores = 0;                 /* Contador del numero de errores        */
@@ -26,7 +27,9 @@ void yywarn( const char * msg )
 /*****************************************************************************/
 int main( int argc, char **argv )
 /* Gestiona la linea de comandos e invoca al analizador sintactico-semantico.*/
-{ int i, n = 0;
+{
+	char *nom_fich;
+	int i, n = 0;
 
 	for ( i = 0; i < argc; ++i ) {
 		if ( strcmp( argv[ i ], "-v" ) == 0 ) { verbosidad = TRUE; n++; }
@@ -38,9 +41,12 @@ int main( int argc, char **argv )
 			fprintf( stderr, "Fichero no valido %s\n", argv[ argc ] );
 		else {
 			if ( verbosidad == TRUE ) fprintf( stdout, "%3d.- ", yylineno );
+			nom_fich = argv[ argc ];
 			yyparse ();
 			if ( numErrores > 0 )
 				fprintf( stdout, "\nNumero de errores:      %d\n", numErrores );
+			else
+				volcarCodigo( nom_fich );
 		}
 	}
 	else fprintf (stderr, "Uso: ./analizador [-v] [-t] fichero\n");
